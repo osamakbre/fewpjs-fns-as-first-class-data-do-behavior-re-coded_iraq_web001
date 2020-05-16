93269b1d95305229d93c0ccee60b81bf30fbb6f0
osama abdullah <osama@re-coded.com>
2020-05-16 08:25:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  timeInt = parseInt(time);
+  if(time < 12){
+    return "Good Morning";
+  } 
+  if(time > 12 && time < 5){
+    return "Good Afternoon";
+  }
+  if(time > 5){
+    return "Good Evening";
+  }
+  
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  const greetings = document.getElementById('greeting')
+  greeting.value = message;
+}
+
